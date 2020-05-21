9ea3b26bd670ab3874e150626b065223707a526c
koashima <48111257+koashima@users.noreply.github.com>
2020-05-20 17:12:12 -0700

    Adds componentDidMount and gets starships from api

---
+++ src/StarshipPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+class StarshipPage extends React.Component { 
+  state = { 
+
+  }
+}
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,7 +1,9 @@
-const ALLSTARSHIPS = 'http https://swapi.dev/api/starships//'
+const ALLSTARSHIPS_ENDPOINT = 'https://swapi.dev/api/starships/'
 
 const getAllStarShips = () => { 
-  
+  return fetch(ALLSTARSHIPS_ENDPOINT)
+    .then( (res) => res.json() )
+    .then( (json) => json.results)
 }
 
 export default getAllStarShips;
