fb23d7241fa7244a5e59f8ecd074b0d9170c4214
koashima <48111257+koashima@users.noreply.github.com>
2020-05-25 18:24:13 -0700

    Adds buttons for starships

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import getAllStarShips from './services/sw-api';
+import StarshipPage from './pages/StarshipPage'
 
 
 
@@ -20,18 +21,35 @@ class App extends React.Component {
 
   getStarships() {
     this.state.starships.map( (starship) => { 
-
+      
     })
   }
 
   render () { 
     return (
-      <div className="App">
+      <div className="App ui container">
         <header className="ui huge header inverted segment">STARWARS</header>
+        <Router>          
+          <Switch>
+            <Route exact path='/' render={() =>
+              <section className='ui container grid centered'>
+                {this.state.starships.map( (starship, idx) => 
+                  <Link 
+                    to={`starships/${idx}`} 
+                    key={starship.name} 
+                    className="massive ui button three wide column"
+                  >
+                    {starship.name}
+                  </Link>
+                )}
+              </section>
+            }/>
+
+          </Switch>
+        </Router>
       </div>
     );
   }
- 
 }
 
 export default App;
--- src/pages/StarshipPage.js
+++ src/pages/StarshipPage.js
@@ -2,7 +2,5 @@ import React from 'react';
 
 
 class StarshipPage extends React.Component { 
-  state = { 
-
-  }
+  
 }
